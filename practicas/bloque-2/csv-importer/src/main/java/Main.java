aba6c1a18ee722e38db69b0c0a1974d66fb58317
Cristhian Ortega <cristhian.aprendizaje@outlook.com>
2026-03-24 23:43:50 -0600

    feat(csv-importer): Update Main class.

--- practicas/bloque-2/csv-importer/src/main/java/Main.java
+++ practicas/bloque-2/csv-importer/src/main/java/Main.java
@@ -1,7 +1,19 @@
 package main.java;
 
+import java.io.IOException;
+
 public class Main {
-    static void main(String[] args) {
-        System.out.println("Hello World!");
+    static void main(String[] args) throws IOException {
+        Importer importer = new Importer();
+        ImportResult result = importer.importProcess(args[0]);
+
+        System.out.println("Correct: " + result.getTransactions().size());
+        System.out.println("Incorrect: " + result.getErrors().size());
+        System.out.println("In: " + result.getTotalIn().getValue().toString());
+        System.out.println("Out: " + result.getTotalOut().getValue().toString());
+
+        for (int i = 0; i < result.getErrors().size(); i++) {
+            System.out.println(result.getErrors().get(i));
+        }
     }
 }
